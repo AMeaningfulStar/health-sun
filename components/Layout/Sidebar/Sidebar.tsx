85020dd0aec74801dd07303d7b82ca965c98a3fd
A Meaningful star <ghrlfehd123@naver.com>
2024-01-23 00:05:08 +0900

    Sidebar에 Navigations 적용

--- components/Layout/Sidebar/Sidebar.tsx
+++ components/Layout/Sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
 import Logo from './Logo'
+import Navgations from './Navgations/Navgations'
 
 export default function Sidebar() {
   return (
     <div className="mr-4 h-screen w-64 py-4">
       <div className="flex h-full w-full flex-col rounded-2xl bg-white">
         <Logo />
+        <Navgations />
       </div>
     </div>
   )
