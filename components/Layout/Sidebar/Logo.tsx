08a1bf3500a7abd622a37e1159a6c6d258a6322f
A Meaningful star <ghrlfehd123@naver.com>
2024-01-23 00:00:27 +0900

    Style: Logo 문구 수정
    24.01.23
    
    - 가운데 정렬을 위한 Logo 문구 padding-rigth 변경

--- components/Layout/Sidebar/Logo.tsx
+++ components/Layout/Sidebar/Logo.tsx
@@ -8,7 +8,7 @@ export default function Logo() {
     <div className="flex h-24 w-full items-center justify-center">
       <Link href={'/'} className="flex flex-row items-center gap-x-2">
         <Lottie loop animationData={LOGO_ICON} play className="aspect-square w-9" />
-        <span className="pr-2 text-2xl text-[#DB8888]">Health sun</span>
+        <span className="pr-6 text-2xl text-[#DB8888]">Health sun</span>
       </Link>
     </div>
   )
