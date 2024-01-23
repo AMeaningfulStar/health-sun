59c4f3a9c4cfec3a8dd0b757ca77c45081bd6f7a
A Meaningful star <ghrlfehd123@naver.com>
2024-01-23 14:02:57 +0900

    Feat: Header Component
    24.01.23

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -1,7 +1,12 @@
+import HeaderBottom from './Bottom/HeaderBottom'
+import HeaderTop from './Top/HeaderTop'
+
 export default function Header() {
-  return(
+  return (
     <div className="h-36 w-full">
-      
+      <HeaderTop />
+      <div className="h-0 w-full border"></div>
+      <HeaderBottom />
     </div>
   )
 }
