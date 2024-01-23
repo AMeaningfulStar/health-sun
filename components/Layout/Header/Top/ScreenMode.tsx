ce6c64b603074388265a22dc8bf030819cf3fe2c
A Meaningful star <ghrlfehd123@naver.com>
2024-01-23 14:57:30 +0900

    Feat: Header의 ScreenMode Component Type
    24.01.23
    
    - ThemeEnum 추가 구현

--- components/Layout/Header/Top/ScreenMode.tsx
+++ components/Layout/Header/Top/ScreenMode.tsx
@@ -1,40 +1,53 @@
 import { useTheme } from 'next-themes'
 import Image from 'next/image'
 
+import { getLogger } from '@libs/logger'
+import { ThemeEnum } from '@libs/theme'
+
 import darkModeIcon from '@img/darkMode.svg'
 import lightModeIcon from '@img/lightMode.svg'
 
 export default function ScreenMode() {
-  const { theme, setTheme } = useTheme()
+  const ModeButton = ({ theme }: { theme: ThemeEnum }) => {
+    const { theme: globalTheme, setTheme } = useTheme()
 
-  const LIGHTMODE_ICON = { src: lightModeIcon, width: 26, height: 26 }
-  const DARKMODE_ICON = { src: darkModeIcon, width: 24, height: 24 }
+    const LIGHTMODE_ICON = { src: lightModeIcon, width: 26, height: 26 }
+    const DARKMODE_ICON = { src: darkModeIcon, width: 24, height: 24 }
+
+    const getImageInfo = (theme: ThemeEnum) => {
+      const logger = getLogger()
+      switch (theme) {
+        case ThemeEnum.Light: {
+          return LIGHTMODE_ICON
+        }
+        case ThemeEnum.Dark: {
+          return DARKMODE_ICON
+        }
+        default: {
+          logger.error('getImageInfo', `theme is not in [${Object.keys(ThemeEnum)}`, theme)
+          return LIGHTMODE_ICON
+        }
+      }
+    }
+
+    const imageInfo = getImageInfo(theme)
+    const isSelected = theme === globalTheme
+
+    return (
+      <div
+        className={`flex h-10 w-10 cursor-pointer items-center justify-center rounded-lg ${isSelected ? 'bg-white' : ''}`}
+        onClick={() => setTheme(theme)}
+      >
+        <Image alt="Screen_Mode:Light" src={imageInfo.src} width={imageInfo.width} height={imageInfo.height} />
+      </div>
+    )
+  }
 
   return (
     <div className="h-14 w-24 rounded-xl bg-gray-200 p-2">
       <div className="flex h-full w-full rounded-lg bg-slate-400">
-        <div
-          className={`flex h-10 w-10 cursor-pointer items-center justify-center rounded-lg ${theme === 'light' ? 'bg-white' : ''}`}
-          onClick={() => setTheme('light')}
-        >
-          <Image
-            alt="Screen_Mode:Light"
-            src={LIGHTMODE_ICON.src}
-            width={LIGHTMODE_ICON.width}
-            height={LIGHTMODE_ICON.height}
-          />
-        </div>
-        <div
-          className={`flex h-10 w-10 cursor-pointer items-center justify-center rounded-lg ${theme === 'dark' ? 'bg-white' : ''}`}
-          onClick={() => setTheme('dark')}
-        >
-          <Image
-            alt="Screen_Mode:Dark"
-            src={DARKMODE_ICON.src}
-            width={DARKMODE_ICON.width}
-            height={DARKMODE_ICON.height}
-          />
-        </div>
+        <ModeButton theme={ThemeEnum.Light} />
+        <ModeButton theme={ThemeEnum.Dark} />
       </div>
     </div>
   )
