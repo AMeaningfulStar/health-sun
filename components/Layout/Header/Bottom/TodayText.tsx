5cff11f51cbd703dd66e71a86638ced6544c625b
A Meaningful star <ghrlfehd123@naver.com>
2024-01-23 14:10:46 +0900

    Feat: TodayText Component
    24.01.23

---
+++ components/Layout/Header/Bottom/TodayText.tsx
@@ -0,0 +1,10 @@
+export default function TodayText() {
+  return (
+    <div className="ml-4 flex flex-col gap-y-px">
+      <span className="text-sm font-semibold text-[#DB8888]">당신을 위한 한 마디</span>
+      <span className="text-xs font-normal text-[#8C8C8C]">
+        이걸 기억하겠다고 약속해줘. 넌 네가 믿는 것보다 더 용감하고, 보기보다 강하고, 네 생각보다 더 똑똑하다는 걸
+      </span>
+    </div>
+  )
+}
