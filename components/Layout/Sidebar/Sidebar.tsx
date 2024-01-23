35f73a86f0b22bdce4fe5452ae1171bd18741d07
A Meaningful star <ghrlfehd123@naver.com>
2024-01-23 13:16:26 +0900

    Sidebar에 Logout 적용

--- components/Layout/Sidebar/Sidebar.tsx
+++ components/Layout/Sidebar/Sidebar.tsx
@@ -1,12 +1,14 @@
 import Logo from './Logo'
+import Logout from './Logout'
 import Navgations from './Navgations/Navgations'
 
 export default function Sidebar() {
   return (
     <div className="mr-4 h-screen w-64 py-4">
-      <div className="flex h-full w-full flex-col rounded-2xl bg-white">
+      <div className="flex h-full w-full flex-col justify-between rounded-2xl bg-white">
         <Logo />
         <Navgations />
+        <Logout />
       </div>
     </div>
   )
