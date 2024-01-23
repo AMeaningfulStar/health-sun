3231d0c7dff5524a2e21b2ef721dead0836cbbfd
A Meaningful star <ghrlfehd123@naver.com>
2024-01-23 14:00:55 +0900

    TODO: ProfileImage Component
    24.01.23

--- components/Layout/Header/Top/ProfileImage.tsx
+++ components/Layout/Header/Top/ProfileImage.tsx
@@ -1,3 +1,8 @@
+/** TODO:
+ *  - 로그인 및 회원 가입 구현 후, user 프로필 이미지 관련 추가 구현 필요
+ *  - 주석 처리 해둔 위치에 이미지 들어가야함
+ */
+
 export default function ProfileImage() {
   return <div className="mr-6 h-12 w-12 rounded-full border-2 bg-slate-500">{/* UserImage 들어갈 곳 */}</div>
 }
