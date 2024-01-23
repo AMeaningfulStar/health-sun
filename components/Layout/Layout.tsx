1e054e5f047c78e01e977c0e4554465a7112ab7c
A Meaningful star <ghrlfehd123@naver.com>
2024-01-23 13:21:06 +0900

    Feat: Layout의 Header 퍼블리싱 틀

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -9,7 +9,14 @@ export default function Layout({ children }: LayoutType) {
     <div className="flex min-h-screen w-full flex-row px-4">
       <Sidebar />
       <div className="flex-grow py-4">
-        <div className="h-full w-full rounded-2xl bg-white">{children}</div>
+        <div className="h-full w-full rounded-2xl bg-white">
+          <div className="h-36 w-full bg-slate-400">
+            <div className='w-full h-1/2'></div>
+            <div className='w-full h-0 border'></div>
+            <div className='w-full h-1/2'></div>
+          </div>
+          {children}
+        </div>
       </div>
     </div>
   )
