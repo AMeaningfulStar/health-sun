f968ee65b555257b734305b8dab3e97c45b05ed1
A Meaningful star <ghrlfehd123@naver.com>
2024-01-23 19:24:00 +0900

    Style: Layout contents div style

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
-import Sidebar from './Sidebar/Sidebar'
 import Header from './Header/Header'
+import Sidebar from './Sidebar/Sidebar'
 
 interface LayoutType {
   children: React.ReactNode
@@ -10,9 +10,9 @@ export default function Layout({ children }: LayoutType) {
     <div className="flex min-h-screen w-full flex-row px-4">
       <Sidebar />
       <div className="flex-grow py-4">
-        <div className="h-full w-full rounded-2xl bg-white">
+        <div className="flex h-full w-full flex-col rounded-2xl bg-white">
           <Header />
-          {children}
+          <div className="mx-auto w-full max-w-6xl flex-grow">{children}</div>
         </div>
       </div>
     </div>
