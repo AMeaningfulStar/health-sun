c5e81c5b187ef74db1e8381718bf139c9f65af44
A Meaningful star <ghrlfehd123@naver.com>
2024-01-23 14:02:20 +0900

    Feat: Header의 ScreenMode Component
    24.01.23
    
    - 'next- theme'를 이용한 시스템 라이트/다크 모드 관리
    - 퍼블리싱을 위한 'darkMode.svg', 'lightMode.svg' 파일 추가

---
+++ components/Layout/Header/Top/ScreenMode.tsx
@@ -0,0 +1,41 @@
+import { useTheme } from 'next-themes'
+import Image from 'next/image'
+
+import darkModeIcon from '@img/darkMode.svg'
+import lightModeIcon from '@img/lightMode.svg'
+
+export default function ScreenMode() {
+  const { theme, setTheme } = useTheme()
+
+  const LIGHTMODE_ICON = { src: lightModeIcon, width: 26, height: 26 }
+  const DARKMODE_ICON = { src: darkModeIcon, width: 24, height: 24 }
+
+  return (
+    <div className="h-14 w-24 rounded-xl bg-gray-200 p-2">
+      <div className="flex h-full w-full rounded-lg bg-slate-400">
+        <div
+          className={`flex h-10 w-10 cursor-pointer items-center justify-center rounded-lg ${theme === 'light' ? 'bg-white' : ''}`}
+          onClick={() => setTheme('light')}
+        >
+          <Image
+            alt="Screen_Mode:Light"
+            src={LIGHTMODE_ICON.src}
+            width={LIGHTMODE_ICON.width}
+            height={LIGHTMODE_ICON.height}
+          />
+        </div>
+        <div
+          className={`flex h-10 w-10 cursor-pointer items-center justify-center rounded-lg ${theme === 'dark' ? 'bg-white' : ''}`}
+          onClick={() => setTheme('dark')}
+        >
+          <Image
+            alt="Screen_Mode:Dark"
+            src={DARKMODE_ICON.src}
+            width={DARKMODE_ICON.width}
+            height={DARKMODE_ICON.height}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
