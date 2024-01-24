ebc8103affd7cfccab0a94748cd72b5d9f2736b8
A Meaningful star <ghrlfehd123@naver.com>
2024-01-24 13:03:38 +0900

    Feat: MainContent  - 'HealthManagement' Component

---
+++ components/MainContents/HealthManagement/HealthManagement.tsx
@@ -0,0 +1,12 @@
+import ContentBox from '../Layout/ContentBox'
+import Title from '../Layout/Element/Title'
+
+export default function HealthManagement() {
+  return (
+    <div className="h-96 w-full">
+      <ContentBox>
+        <Title title='나의 건강 관리' />
+      </ContentBox>
+    </div>
+  )
+}
