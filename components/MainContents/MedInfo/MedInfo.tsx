790cb9b674a48977b5314aac5490af0c9cae9d81
A Meaningful star <ghrlfehd123@naver.com>
2024-01-24 13:33:16 +0900

    Feat: MainContent  - 'MedInfo' Component

---
+++ components/MainContents/MedInfo/MedInfo.tsx
@@ -0,0 +1,12 @@
+import ContentBox from '../Layout/ContentBox'
+import Title from '../Layout/Element/Title'
+
+export default function MedInfo() {
+  return (
+    <div className="h-[19rem] w-full">
+      <ContentBox>
+        <Title title="복약 정보" />
+      </ContentBox>
+    </div>
+  )
+}
