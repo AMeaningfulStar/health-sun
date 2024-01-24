9e5ef92b8873f8beae73cc7f4b70a0b79f2ffb12
A Meaningful star <ghrlfehd123@naver.com>
2024-01-24 13:04:13 +0900

    Feat: MainContent  - 'VaccinationRecommendation' Component

---
+++ components/MainContents/VaccinationRecommendation/VaccinationRecommendation.tsx
@@ -0,0 +1,12 @@
+import ContentBox from '../Layout/ContentBox'
+import Title from '../Layout/Element/Title'
+
+export default function VaccinationRecommendation(){
+  return (
+    <div className="h-60 w-full">
+      <ContentBox>
+        <Title title='예방 접종 추천' />
+      </ContentBox>
+    </div>
+  )
+}
