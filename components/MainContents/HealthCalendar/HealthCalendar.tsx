0010e14488ec6f1712b6fafee235b6c9399f61ef
A Meaningful star <ghrlfehd123@naver.com>
2024-01-24 13:33:04 +0900

    Feat: MainContent  - 'HealthCalendar' Component

---
+++ components/MainContents/HealthCalendar/HealthCalendar.tsx
@@ -0,0 +1,12 @@
+import ContentBox from '../Layout/ContentBox'
+import Title from '../Layout/Element/Title'
+
+export default function HealthCalendar() {
+  return (
+    <div className="h-80 w-full">
+      <ContentBox>
+        <Title title="건강 캘린더" />
+      </ContentBox>
+    </div>
+  )
+}
