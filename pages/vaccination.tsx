119c88f8e937a6623ab11ab561a2f94f12e869e1
A Meaningful star <ghrlfehd123@naver.com>
2024-01-30 14:56:33 +0900

    Feat: 'Vaccination' component - content Layout

--- pages/vaccination.tsx
+++ pages/vaccination.tsx
@@ -8,6 +8,8 @@ export default function Vaccination() {
         <div className="mb-4 w-full">
           <span className=" text-2xl font-semibold text-gray-500">예방 접종 추천</span>
         </div>
+        <div className="flex flex-row items-center  justify-between gap-x-4">
+        </div>
       </div>
     </Layout>
   )
